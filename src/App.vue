b3371b343d11f0ae990b7713aeed20a4406d8ae6
alberto-2-garcia <alberto.2.garcia@atos.net>
2022-11-08 22:13:46 -0800

    Cree componente Products

--- src/App.vue
+++ src/App.vue
@@ -7,56 +7,33 @@
     </header>
 
     <div v-if="page === 'cart'">
-      <h1> Carritos </h1>
+      <h1> Carrito </h1>
       <div class="products">
-        <div v-for="product in cart" :key="product.name">
+        <div v-for="(product, index) in cart" :key="index">
         {{product.name}}
         <img :src="product.image">
         <div>{{product.cost}}</div>
-        <button v-on:click="addItemToCart"> Agregar a carrito</button>
+        <button v-on:click="removeItemFromCart"> Quitar del carrito</button>
         </div>
       </div>
     </div>
-  </div>
 
     <div v-if="page === 'products'">
-      <h1> Productos </h1>
-      <div class="products">
-        <div v-for="product in products" :key="product.name">
-        {{product.name}}
-        <img :src="product.image">
-        <div>{{product.cost}}</div>
-        <button v-on:click="addItemToCart"> Agregar a carrito</button>
-        </div>
-      </div>
+      <Products v-on:addItemToCart="addItemToCart"/>
     </div>
+  </div>
   
 </template>
 
 <script>
+import Products from './components/Products.vue'
+
 export default{
   name: "App",
   data:() => {
     return{
       page: 'products',
-      cart:[],
-      products:[
-      {
-        name: 'Agua Ciel Exprim Pina Jengibre 1L',
-        cost: '$78.00',
-        image: 'https://www.coca-colaentuhogar.com/media/catalog/product/c/i/ciel_xprim_vis2018_pi_a_jengibre_pet1ml_seco_1_optimized_4.jpg?optimize=medium&fit=bounds&height=175&width=175&canvas=175:175'
-      },
-      {
-        name: 'Agua Ciel Exprim Fresa 1L',
-        cost: '$14.00',
-        image: 'https://www.coca-colaentuhogar.com/media/catalog/product/3/9/39_ciel-exprim-fresa-1-l-botella-pet_5_1.jpg?optimize=medium&fit=bounds&height=175&width=175&canvas=175:175'
-      },
-      {
-        name: 'Agua Ciel Exprim Jamaica 1.5L',
-        cost: '$95.00',
-        image: 'https://www.coca-colaentuhogar.com/media/catalog/product/4/5/45_ciel-exprim-jamaica-reposada-1.5-l-botella-pet_5.jpg?optimize=medium&fit=bounds&height=175&width=175&canvas=175:175'
-      }
-    ]
+      cart:[]
     };
   },
   methods :{
@@ -64,12 +41,17 @@ export default{
       this.cart.push(product);
     },
 
+    removeItemFromCart(product){
+      this.cart.splice(this.cart.indexOf(product));
+    },
+
     navigateTo(page){
       this.page = page;
-      this.cart.push(product);
     }
   },
-  components :{}
+  components :{
+    Products
+  }
 };
 </script>
 
---
+++ src/components/Products.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <h1> Productos </h1>
+    <div class="products">
+    <div v-for="(product, index) in products" :key="index">
+    {{product.name}}
+    <img :src="product.image">
+    <div>{{product.cost}}</div>
+    <button v-on:click="addItemToCart"> Agregar a carrito</button>
+    </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default{
+    data: () =>  {
+        return {
+            products:[
+                {
+                name: 'Agua Ciel Exprim Pina Jengibre 1L',
+                cost: '$78.00',
+                image: 'https://www.coca-colaentuhogar.com/media/catalog/product/c/i/ciel_xprim_vis2018_pi_a_jengibre_pet1ml_seco_1_optimized_4.jpg?optimize=medium&fit=bounds&height=175&width=175&canvas=175:175'
+                },
+                {
+                name: 'Agua Ciel Exprim Fresa 1L',
+                cost: '$14.00',
+                image: 'https://www.coca-colaentuhogar.com/media/catalog/product/3/9/39_ciel-exprim-fresa-1-l-botella-pet_5_1.jpg?optimize=medium&fit=bounds&height=175&width=175&canvas=175:175'
+                },
+                {
+                name: 'Agua Ciel Exprim Jamaica 1.5L',
+                cost: '$95.00',
+                image: 'https://www.coca-colaentuhogar.com/media/catalog/product/4/5/45_ciel-exprim-jamaica-reposada-1.5-l-botella-pet_5.jpg?optimize=medium&fit=bounds&height=175&width=175&canvas=175:175'
+                }
+            ]       
+        };
+    },
+    methods:{
+        addItemToCart(product){
+            this.$emit('addItemToCart',product);
+        }
+    }
+};
+</script>
+
+<style>
+</style>
